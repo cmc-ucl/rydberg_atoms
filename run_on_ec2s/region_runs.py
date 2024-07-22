7256465e304215947b78fd82ef5eda90912e4135
maolinml <86260930+maolinml@users.noreply.github.com>
2024-07-21 21:09:49 -0700

    add failed test and train sets

--- run_on_ec2s/region_runs.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-regions = [
-    'eu-west-2', 'ap-northeast-3', 'ap-southeast-1', 'eu-central-1', 'eu-west-1', 'eu-west-3'
-]
-
-for region in regions:
-    subprocess.run(['python', 'run_545821822555.py', region])
